3413224a69dda142b1858f5c1b80a8b3d8dcc5c2
Ksenia Skopiuk <ksenia.agag@gmail.com>
2023-01-31 16:54:07 +0000

    migration and dependencies was added

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"coffeecatalogue4"
 	"coffeecatalogue4/pkg/handler"
+	"coffeecatalogue4/pkg/repository"
+	"coffeecatalogue4/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := InitConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(coffeecatalogue4.Server)
-	if err := srv.Run("8080", handlers.InitRouters()); err != nil {
+	if err := srv.Run(viper.GetString("path"), handlers.InitRouters()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
